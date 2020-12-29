a6cf883decf6669bb294ccec8542444f872ccb26
CataChavez <kata.chavez@gmail.com>
2020-12-29 14:19:27 -0300

    CRUD

--- db/index.js
+++ db/index.js
@@ -14,17 +14,42 @@ async function getDate() {
   const result = await pool.query("SELECT NOW()")
   return result
 }
-//dataArray example => ['lagartijas', 3, 15, 3]
-async function createExercises(dataArray) {
+//paramsArray example => ['lagartijas', 3, 15, 3]
+async function createExercise(paramsArray) {
   const qryObject = {
-    text: 'INSERT INTO excercises (name, series, repititions, rest) VALUES ($1, $2, $3, $4)',
-    values: dataArray
+    text: 'INSERT INTO exercises (name, series, repetitions, rest) VALUES ($1, $2, $3, $4)',
+    values: paramsArray
   }
   const result = await pool.query(qryObject)
   return result
 }
 
+async function getExercise(){
+  const result = await pool.query('SELECT * FROM exercises')
+  return result
+}
+
+//paramsArray example => ['lagartijas', 3, 15, 3]
+async function updateExercise(paramsArray){
+  //TODO: flexible params
+  const qryObject = {
+    text:
+      "UPDATE exercises SET name = $1, series = $2, repetitions = $3, rest = $4 WHERE name = $1 RETURNING *",
+    values: paramsArray,
+  };
+  const result = await pool.query(qryObject);
+  return result;
+}
+
+async function destroyExercise(name){
+  const result = await pool.query(`DELETE FROM exercises WHERE name = '${name}'`)
+  return result
+}
+
 module.exports = {
   getDate,
-  createExercises
+  createExercise,
+  getExercise,
+  updateExercise,
+  destroyExercise
 }
